d308b87e3071017ed9322fc8fcf971548d34dd29
JordiRon <jordi.ron@uvic.cat>
2014-02-20 18:08:27 +0100

    initial commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by jordi.ron on 20/02/14.
+//  Copyright (c) 2014 jordi.ron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by jordi.ron on 20/02/14.
+//  Copyright (c) 2014 jordi.ron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
